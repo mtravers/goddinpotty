a97761dba03a163986b15c88b78bd74662eacce7
Mike Travers <mtravers@parkerici.org>
2022-10-09 16:51:24 -0700

    Finally a version of update script that works right (mostly)

--- bin/update.sh
+++ bin/update.sh
@@ -1,10 +1,8 @@
-# Run before any Logseq generate, quits Logseq to force graph out
-# TODO something better than runs from nbb-logseq and doesn't quit
-# TODO turned off, it doesn't even work (that is, not all changes get saved to the db, would need to do a reindex I guess which is slow and I don't know if its applescriptable.)
-# osascript bin/update.script
-# TODO this doesn't work either, probably needs to wait for old one to quit, not sure how to do that.
-# open /Applications/Logseq.app	# tried to do this with applscript, doesn't really work
+# MacOS only
+# TODO relies on there being an open Logseq window THAT IS OPEN to the right graph, which is bad
+# Run before any Logseq generate, forces a C-x C-s (save graph command) to Logseq
+# see also https://github.com/logseq/nbb-logseq/issues/1
 
-echo "You need to hit C-x C-s (save current graph to disk) in Logseq"
-# TODO see https://github.com/logseq/nbb-logseq/issues/1
+osascript bin/update.script
+
 
